1bd317a774fba55c0a27af3586b247378910ed1c
Arun <ap0170462@gmail.com>
2021-05-21 18:34:30 +0530

    smalll fixbilty

--- src/main/java/com/moviepur/servies/impl/AllServiceImpl.java
+++ src/main/java/com/moviepur/servies/impl/AllServiceImpl.java
@@ -88,7 +88,6 @@ public class AllServiceImpl implements AllService {
 
 	@Override
 	public List<Object> getFormatedDateForAndroid() {
-		System.out.println(movieLiteRepository.getAllIndustryByGenre("action"));
 		Set<String> genres =	getAllGenres();
 		List<Object> result = new LinkedList<>();
 		genres.forEach(x -> {
@@ -97,6 +96,7 @@ public class AllServiceImpl implements AllService {
 			map.put("data", movieLiteRepository.getAllIndustryByGenre(x));
 			result.add(map);
 		});
+		System.out.println(result);
 		return result;
 	}
 
