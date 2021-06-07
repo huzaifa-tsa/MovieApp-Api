38e3be2198cfbda17f0dd832c21c9bcc78732a3a
Arun Patel <60776920+ArunPatel1999@users.noreply.github.com>
2021-06-07 20:37:11 +0530

    Testing

--- src/main/java/com/moviepur/FirebaseConfig.java
+++ src/main/java/com/moviepur/FirebaseConfig.java
@@ -1,7 +1,34 @@
 package com.moviepur;
 
+// import javax.annotation.PostConstruct;
+// import org.springframework.context.annotation.Configuration;
+// import com.google.auth.oauth2.GoogleCredentials;
+// import com.google.firebase.FirebaseApp;
+// import com.google.firebase.FirebaseOptions;
+
+// @Configuration
+// public class  {
+	
+// 	@PostConstruct
+// 	public void firebaseSetup() {
+// 		try {
+// 			FirebaseOptions options = FirebaseOptions.builder().setCredentials(
+// 					GoogleCredentials.fromStream(this.getClass().getResourceAsStream("/moviepur_firebase.json")))
+// 					.build();
+
+// 			FirebaseApp.initializeApp(options);
+// 		} catch (Exception e) {
+// 			e.printStackTrace();
+// 		}
+// 	}
+	
+// }
+
+
 import javax.annotation.PostConstruct;
+
 import org.springframework.context.annotation.Configuration;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -9,17 +36,20 @@ import com.google.firebase.FirebaseOptions;
 @Configuration
 public class FirebaseConfig {
 	
-	@PostConstruct
+    @PostConstruct
 	public void firebaseSetup() {
 		try {
-			FirebaseOptions options = FirebaseOptions.builder().setCredentials(
-					GoogleCredentials.fromStream(this.getClass().getResourceAsStream("/moviepur_firebase.json")))
-					.build();
-
-			FirebaseApp.initializeApp(options);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			FirebaseOptions options = new FirebaseOptions.Builder()
+					.setCredentials(GoogleCredentials.fromStream(this.getClass().getResourceAsStream("/moviepur_firebase.json")))
+                    .build();
+			if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            } else {
+               FirebaseApp.getInstance();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
-	
+
 }
