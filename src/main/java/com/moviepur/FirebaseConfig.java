58527db31fdebd95ad703d590c1e1b90e24c75c0
Arun Patel <60776920+ArunPatel1999@users.noreply.github.com>
2021-06-07 21:04:36 +0530

    zzz

--- src/main/java/com/moviepur/FirebaseConfig.java
+++ src/main/java/com/moviepur/FirebaseConfig.java
@@ -1,6 +1,6 @@
 package com.moviepur;
 
-import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -9,7 +9,7 @@ import com.google.firebase.FirebaseOptions;
 @Configuration
 public class FirebaseConfig  {
 	
-	@PostConstruct
+	@Bean
 	public void firebaseSetup() {
 		try {
 			FirebaseOptions options = FirebaseOptions.builder().setCredentials(
