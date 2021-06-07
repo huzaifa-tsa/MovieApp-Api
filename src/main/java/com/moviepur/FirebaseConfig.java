d60a74642b90919b3a075297019322f4db16cace
Arun Patel <60776920+ArunPatel1999@users.noreply.github.com>
2021-06-07 20:40:29 +0530

    sos

--- src/main/java/com/moviepur/FirebaseConfig.java
+++ src/main/java/com/moviepur/FirebaseConfig.java
@@ -1,55 +1,26 @@
 package com.moviepur;
 
-// import javax.annotation.PostConstruct;
-// import org.springframework.context.annotation.Configuration;
-// import com.google.auth.oauth2.GoogleCredentials;
-// import com.google.firebase.FirebaseApp;
-// import com.google.firebase.FirebaseOptions;
-
-// @Configuration
-// public class  {
-	
-// 	@PostConstruct
-// 	public void firebaseSetup() {
-// 		try {
-// 			FirebaseOptions options = FirebaseOptions.builder().setCredentials(
-// 					GoogleCredentials.fromStream(this.getClass().getResourceAsStream("/moviepur_firebase.json")))
-// 					.build();
-
-// 			FirebaseApp.initializeApp(options);
-// 		} catch (Exception e) {
-// 			e.printStackTrace();
-// 		}
-// 	}
-	
-// }
-
-
 import javax.annotation.PostConstruct;
-
 import org.springframework.context.annotation.Configuration;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
 @Configuration
-public class FirebaseConfig {
+public class  {
 	
-    @PostConstruct
+	@PostConstruct
 	public void firebaseSetup() {
 		try {
-			FirebaseOptions options = new FirebaseOptions.Builder()
-					.setCredentials(GoogleCredentials.fromStream(this.getClass().getResourceAsStream("/moviepur_firebase.json")))
-                    .build();
-			if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            } else {
-               FirebaseApp.getInstance();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-	}
+			FirebaseOptions options = FirebaseOptions.builder().setCredentials(
+					GoogleCredentials.fromStream(this.getClass().getResourceAsStream("/moviepur_firebase.json")))
+					.build();
 
+			FirebaseApp.initializeApp(options);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
+
